2a437531cb53005ccba3482f5d913ad2bd83c798
Daniel <daniel@192.168.1.101>
2026-04-01 21:57:42 +0300

    GetProducts

--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductEndpoint.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductEndpoint.cs
@@ -1,6 +1,19 @@
 namespace Catalog.API.Products.GetProducts;
+public record GetProductResponse(IEnumerable<Product> Products);
 
-public class GetProductEndpoint
+public class GetProductEndpoint : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            var response = result.Adapt<GetProductResponse>();
+            return Results.Ok(response);
+        })
+         .WithName("GetProduct")
+        .Produces<GetProductsResults>(StatusCodes.Status201Created)
+        .WithSummary("Get Product")
+        .WithDescription("Get Product");;
+    }
 }
