9abf979c3d8803a38729037dc306a6ce411d06a3
Baron Daniel <baron.daniel.86@gmail.com>
2026-04-07 10:16:44 +0300

    Pagination

--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductEndpoint.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductEndpoint.cs
@@ -1,13 +1,16 @@
 namespace Catalog.API.Products.GetProducts;
+
+public record GetProductRequest(int? PageNumber = 1, int? PageSize = 10);
 public record GetProductResponse(IEnumerable<Product> Products);
 
 public class GetProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/products", async (ISender sender) =>
-        {
-            var result = await sender.Send(new GetProductsQuery());
+        app.MapGet("/products", async ([AsParameters] GetProductRequest request, ISender sender) =>
+            {
+            var queryable = request.Adapt<GetProductsQuery>();
+            var result = await sender.Send(queryable);
             var response = result.Adapt<GetProductResponse>();
             return Results.Ok(response);
         })
--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,6 +1,6 @@
 namespace Catalog.API.Products.GetProducts;
 
-public record GetProductsQuery() : IQuery<GetProductsResults>;
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResults>;
 public record GetProductsResults(IEnumerable<Product> Products);
 
 internal class GetProductsQueryHandler(IDocumentSession session)
@@ -8,7 +8,7 @@ internal class GetProductsQueryHandler(IDocumentSession session)
 {
     public async Task<GetProductsResults> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        var products = await session.Query<Product>().ToPagedListAsync(query.PageNumber ?? 1 ,query.PageSize ?? 10,cancellationToken);
         return new GetProductsResults(products);
 
     }
