649d6582cc433d6aa5f9b4f17c7a8f56dd6092f3
Baron Daniel <baron.daniel.86@gmail.com>
2026-04-10 20:29:36 +0300

    GetBasket

--- eshop-microservices/src/Services/Basket/Basket.API/GlobalUsing.cs
+++ eshop-microservices/src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -1,2 +1,4 @@
 global using Basket.API.Models;
 global using BuildingBlocks.CQRS;
+global using Carter;
+global using MediatR;
--- eshop-microservices/src/Services/Basket/Basket.API/Program.cs
+++ eshop-microservices/src/Services/Basket/Basket.API/Program.cs
@@ -1,11 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 //ADD Services to container
-
+var assemblies = typeof(Program).Assembly;
+builder.Services.AddCarter(new DependencyContextAssemblyCatalog(
+    new[] { assemblies }
+));
 
 var app = builder.Build();
 
 //Configure the HTTP Request pipeline
-
+app.MapCarter();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
