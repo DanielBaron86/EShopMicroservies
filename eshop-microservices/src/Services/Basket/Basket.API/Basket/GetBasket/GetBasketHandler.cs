6038a7b8d78805a9370c2fb87a4dcb01a313e0e7
Baron Daniel <baron.daniel.86@gmail.com>
2026-04-10 14:30:49 +0300

    Basket Get TBC

--- eshop-microservices/src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ eshop-microservices/src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,6 +1,12 @@
 namespace Basket.API.Basket.GetBasket;
-
-public class GetBasketHandler
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+public record GetBasketResult(ShoppingCart Cart);
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery,GetBasketResult>
 {
-    
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        //TODO: get basket from database
+        //var basket =await _repository.GetBasket(request.Username);
+        return new GetBasketResult( new ShoppingCart("test") );
+    }
 }
