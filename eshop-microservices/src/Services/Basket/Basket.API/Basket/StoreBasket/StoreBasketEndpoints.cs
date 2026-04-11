89a400fd0b04ef615b22f7a32ae13999898213a8
Baron Daniel <baron.daniel.86@gmail.com>
2026-04-11 17:51:44 +0300

    StoreBasket

--- eshop-microservices/src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
+++ eshop-microservices/src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
@@ -1,6 +1,23 @@
-namespace Basket.API.Basket.StoreBasket;
+using Mapster;
 
-public class StoreBasketEndpoints
+namespace Basket.API.Basket.StoreBasket;
+public record StoreBasketRequest(ShoppingCart Cart);
+public record StoreBasketResponse(string UserName);
+public class StoreBasketEndpoints : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("basket", async (ISender sender,StoreBasketRequest request) =>
+        {
+            var command = request.Adapt<StoreBasketCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<StoreBasketResponse>();
+            return Results.Created($"/basket/{response.UserName}",response);
+        })
+        .WithName("StoreBasket")
+        .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+        .WithSummary("Store Basket By Request")
+        .WithDescription("Stores the user's shopping cart in the system.");
+            ;
+    }
 }
--- eshop-microservices/src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ eshop-microservices/src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,22 @@
 namespace Basket.API.Basket.StoreBasket;
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string userName);
 
-public class StoreBasketHandler
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 {
-    
+    public StoreBasketCommandValidator()
+    {
+     RuleFor(x => x.Cart).NotNull().NotEmpty().WithMessage("Cart is required");
+     RuleFor(x => x.Cart.UserName).NotNull().NotEmpty().WithMessage("Username is required");
+    }
+}
+
+public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand,StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand commnad, CancellationToken cancellationToken)
+    {
+        //TODO: save basket to database(Marten Upsert)
+        //TODO: update cache
+        return new StoreBasketResult(commnad.Cart.UserName);
+    }
 }
