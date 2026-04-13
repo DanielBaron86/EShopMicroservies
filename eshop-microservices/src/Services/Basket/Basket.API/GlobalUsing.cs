b23ddfc79f5dd699322af24819cc8eb3423973a3
Baron Daniel <baron.daniel.86@gmail.com>
2026-04-13 08:40:46 +0300

    CachedBasket

--- eshop-microservices/src/Services/Basket/Basket.API/GlobalUsing.cs
+++ eshop-microservices/src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -12,3 +12,4 @@ global using Marten;
 global using BuildingBlocks.Exceptions;
 global using Basket.API.Exceptions;
 global using Basket.API.Data;
+global using Microsoft.Extensions.Caching.Distributed;
